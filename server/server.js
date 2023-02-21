e8f2277850db63c32fb8602feef066ed1422fd0e
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2023-02-21 22:33:04 +0530

    add CORS, middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
-import express from 'express';
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 
 const app = express();
-
-const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
