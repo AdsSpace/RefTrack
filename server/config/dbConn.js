e26e56b471f7d3a6a6ce8ab041176e0ba09c454f
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2023-03-03 00:56:27 +0530

    add enpoint to add, update, patch and delete user

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
   try {
+    mongoose.set('strictQuery', true);
     await mongoose.connect(process.env.DATABASE_URL);
   } catch (error) {
     console.log(error);
